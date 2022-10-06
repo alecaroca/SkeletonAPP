fd16e0d4b06dc765b74a8ea9eee258fdb5707ccf
Alejandro Caroca <ale.caroca@duocuc.cl>
2022-10-06 18:16:15 -0300

    geolocalizacion y Google maps

--- src/app/geo/geo.page.ts
+++ src/app/geo/geo.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Geolocation, Geoposition } from '@awesome-cordova-plugins/geolocation/ngx';
 import { NavController } from '@ionic/angular';
+import { ViewChild,ElementRef } from '@angular/core';
 
+declare var google: any;
 @Component({
   selector: 'app-geo',
   templateUrl: './geo.page.html',
@@ -9,19 +11,48 @@ import { NavController } from '@ionic/angular';
 })
 export class GeoPage  {
 
+  map: any;
+  //variable para recoger la geolocalizacion
+  xy: any;
+  marker:any;
+  @ViewChild('map',{read: ElementRef, static: false}) mapRef: ElementRef;  
+
   constructor(public geolocation:Geolocation, navCtrl: NavController) { }
 
-  ngAfterViewInit(){
-    this.geoNative();
-  }
-
+   //capturo mi localizacion con api nativa de ionic
   geoNative(){
     this.geolocation.getCurrentPosition().then((geoposition :Geoposition)=>{
-
+      this.xy = geoposition;
       console.log(geoposition);
     })
   }
 
+ 
+  //Funcion que crea el mapa
+  showMap(){
+    
+    const flecha ={lat: this.xy.coords.latitude, lng:this.xy.coords.longitude};
+    const location = new google.maps.LatLng(this.xy.coords.latitude, this.xy.coords.longitude);
+    const options = {
+      center: location,
+      zoom: 18,
+      button: "ok",
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+    //Marcador
+    var marker = new google.maps.Marker({
+      position: {lat: this.xy.coords.latitude, lng: this.xy.coords.longitude},
+      map: this.map,
+      title: "Tu ubicacion"
+        });
+  }
 
+  ionViewDidEnter(){
+    this.showMap();
+  }
 
+  ngAfterViewInit(){
+    this.geoNative();
+  }
+ 
 }
